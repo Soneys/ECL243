817beccf1a8310a5cf6cb73976f2b78b4f4d5925
Soneys <33501155+Soneys@users.noreply.github.com>
2019-03-13 15:46:41 -0700

    upload

---
+++ run_kallisto_dev.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for f1 in *_1_val_1.fq.gz
+do
+	echo ${f1%%_1_val_1.fq.gz}
+	f2=${f1%%_1_val_1.fq.gz}"_2_val_2.fq.gz"
+	kallisto quant --rf-stranded -i dev_transcriptome.idx -o kallisto_outs/${f1%%_1_val_1.fq.gz} $f1 $f2
+done
